3242e22ffa4d61a54ce080b2aef11b128c83ef4c
Yukta <yukta.sachdev@hexaviewtech.com>
2025-11-12 23:38:55 +0530

    deploy ci/cd yaml created

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/fleet-dashboard/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
